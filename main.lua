889f0a15942d9acca9638116bf5205504521d16b
TheSailor1 - Apple <157094447+TheSailor1@users.noreply.github.com>
2024-07-26 08:46:42 +0200

    Initialize

---
+++ main.lua
@@ -0,0 +1,16 @@
+love = require('love')
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
